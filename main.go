0aca64dfb5298fb00e9166e52cc99f558c0f60a8
Dan Chen <dan.chen@boardware.com>
2023-08-17 16:17:57 +0800

    fixup

--- main.go
+++ main.go
@@ -1,12 +1,57 @@
 package main
 
 import (
+	"errors"
+	"net/smtp"
+
 	"github.com/boardware-cloud/common/config"
 	"github.com/boardware-cloud/core/controllers"
 	_ "github.com/boardware-cloud/core/services"
 )
 
+type loginAuth struct {
+	username, password string
+}
+
+// LoginAuth is used for smtp login auth
+func LoginAuth(username, password string) smtp.Auth {
+	return &loginAuth{username, password}
+}
+
+func (a *loginAuth) Start(server *smtp.ServerInfo) (string, []byte, error) {
+	return "LOGIN", []byte(a.username), nil
+}
+
+func (a *loginAuth) Next(fromServer []byte, more bool) ([]byte, error) {
+	if more {
+		switch string(fromServer) {
+		case "Username:":
+			return []byte(a.username), nil
+		case "Password:":
+			return []byte(a.password), nil
+		default:
+			return nil, errors.New("Unknown from server")
+		}
+	}
+	return nil, nil
+}
+
 func main() {
+	user := config.GetString("smtp.email")
+	from := config.GetString("smtp.email")
+	password := config.GetString("smtp.password")
+	to := []string{"chenyunda218@gmail.com"}
+	smtpHost := config.GetString("smtp.host")
+	smtpPort := config.GetString("smtp.port")
+
+	message := []byte("Hello! I'm trying out smtp to send emails to recipients.")
+
+	// Create authentication
+	auth := LoginAuth(user, password)
+
+	// Send actual message
+	smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+
 	port := ":" + config.GetString("server.port")
 	controllers.Init()
 	controllers.Run(port)

90cb5620a71455428df1e0183e735fd4f1555255
Dan Chen <dan.chen@boardware.com>
2023-08-17 10:45:36 +0800

    feat

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
-FROM golang:1.20
+FROM golang:1.20 as build-stage
 WORKDIR /app
 COPY go.mod go.sum ./
 RUN go mod download
 COPY . .
 RUN go build -o /main
+
+FROM ubuntu:22.04 as production-stage
+COPY --from=build-stage /main /main
 EXPOSE 8080
 CMD [ "/main" ]
--- controllers/init.go
+++ controllers/init.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/boardware-cloud/common/server"
 	api "github.com/boardware-cloud/core-api"
 	"github.com/gin-gonic/gin"
@@ -8,9 +10,26 @@ import (
 
 var router *gin.Engine
 
+type health struct {
+	Status string   `json:"status"`
+	Checks []string `json:"checks"`
+}
+
 func Init() {
 	router = gin.Default()
 	router.Use(server.CorsMiddleware())
+	router.GET("/health/ready", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, health{
+			Status: "UP",
+			Checks: make([]string, 0),
+		})
+	})
+	router.GET("/health/live", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, health{
+			Status: "UP",
+			Checks: make([]string, 0),
+		})
+	})
 	api.AccountApiInterfaceMounter(router, accountApi)
 	api.ServicesApiInterfaceMounter(router, serviceApi)
 	api.VerificationApiInterfaceMounter(router, verificationApi)

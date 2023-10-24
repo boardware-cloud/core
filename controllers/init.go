e2eb85474f8579e70381d019f2384f99803d6d7b
Dan Chen <dan.chen@boardware.com>
2023-10-24 14:01:12 +0800

    feat

--- controllers/init.go
+++ controllers/init.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	api "github.com/boardware-cloud/core-api"
 	coreServices "github.com/boardware-cloud/core/services"
 	"github.com/boardware-cloud/middleware"
@@ -17,7 +19,22 @@ func Init(inject *gorm.DB) {
 	accountService = coreServices.NewAccountService(inject)
 	router = gin.Default()
 	router.Use(accountService.Auth())
-	router.Use(middleware.CorsMiddleware())
+	router.Use(func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+		if origin != "" {
+			c.Header("Access-Control-Allow-Origin", "*") // 可将将 * 替换为指定的域名
+			c.Header("Access-Control-Allow-Methods", "*")
+			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		c.Next()
+	})
+	// router.Use(middleware.CorsMiddleware())
 	middleware.Health(router)
 	var accountApi AccountApi
 	api.AccountApiInterfaceMounter(router, accountApi)
